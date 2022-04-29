515b1d3324679b458adcfa990f479d8cf72584ad
Ксения Гостева <64367193+XenGost@users.noreply.github.com>
2022-04-29 15:38:59 +0400

    feat: complete task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(array) {
+  let transform_array =[].concat(array);
+    let index = 0;
+    if (array.includes('--discard-next',0)){
+        index = array.indexOf('--discard-next',0);
+        transform_array.splice(index+1, 1);
+    }
+    if (array.includes('--discard-prev',0)){
+        index = array.indexOf('--discard-prev',0);
+        transform_array.splice(index-1, 1);
+    }
+    if (array.includes('--double-next',0)){
+        index = array.indexOf('--double-next',0);
+        transform_array[index]=array[index+1];
+    }
+    if (array.includes('--double-prev',0)){
+        index = array.indexOf('--double-prev',0);
+        transform_array[index]=array[index-1];
+    }
+
+return transform_array;
 }
 
 module.exports = {
