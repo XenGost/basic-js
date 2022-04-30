54c28265afeec3666b0f475f5dea01dd2c306a16
Ксения Гостева <64367193+XenGost@users.noreply.github.com>
2022-04-30 11:22:12 +0400

    feat: complete task

--- src/transform-array.js
+++ src/transform-array.js
@@ -15,25 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function transform(array) {
   let transform_array =[].concat(array);
-   if (transform_array.length==1){
+    if (transform_array.length==1){
         return '\'arr\' parameter must be an instance of the Array!'
     }
     let index = 0;
     if (array.includes('--discard-next',0)){
+        if(index!=array.length-1){
         index = array.indexOf('--discard-next',0);
         transform_array.splice(index, 2);
+        }
+        
     }
     if (array.includes('--discard-prev',0)){
+        if(index!=0){
         index = array.indexOf('--discard-prev',0);
         transform_array.splice(index-1, 2);
+        }
+        
     }
     if (array.includes('--double-next',0)){
+         if(index!=array.length-1){
         index = array.indexOf('--double-next',0);
         transform_array[index]=array[index+1];
+         }
     }
     if (array.includes('--double-prev',0)){
+         if(index!=0){
         index = array.indexOf('--double-prev',0);
         transform_array[index]=array[index-1];
+         }
+         
     }
 
 return transform_array;
